a735c1ef91f7ccedd11cb8398471d16fc686a91e
Marcell Alves <marcell.alves@gmail.com>
2020-01-17 23:05:28 -0300

    Refatorando arquivos docker-compose Alterando o título da aplicação no Swagger

--- Dito.Autocomplete/Startup.cs
+++ Dito.Autocomplete/Startup.cs
@@ -34,7 +34,7 @@ namespace Dito.Autocomplete
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dito Autocomplete", Version = "v1" });
             });
         }
 
@@ -60,7 +60,7 @@ namespace Dito.Autocomplete
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dito Autocomplete");
             });
         }
     }
